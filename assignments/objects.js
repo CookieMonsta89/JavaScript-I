1a7d969a7c96766e699ec6be58c65f91f56c99cb
CookieMonsta89 <jlstan06@gmail.com>
2018-09-18 14:22:42 -0400

    finished

--- assignments/objects.js
+++ assignments/objects.js
@@ -1,64 +1,36 @@
-// Let's get some practice writing a few objects for a new group of interns at a small business.
+// ==== Objects ====
 
-// ==== Challenge 1: Writing Objects ==== 
-// HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
+/* 
+  Given the following information about dinosaurs, create 3 objects: 
+  Use this pattern to create your objects: 
+  object name, diet, weight, length, period
+*/
 
-// 1,mmelloy0@psu.edu,Mitzi,F
-// 2,kdiben1@tinypic.com,Kennan,M
-// 3,kmummery2@wikimedia.org,Keven,M
-// 4,gmartinson3@illinois.edu,Gannie,M
-// 5,adaine5@samsung.com,Antonietta,F
+// tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
+const tyrannosaurus = {
+  name: 'T-Rex',
+  diet: 'Carnivore',
+  roar: () => {
+    return 'Hello, Im a trex'
 
-// Example format of an intern object: 1,examples@you.edu,Example,F
-const example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
-  "gender": "F"
+  }
 }
+// stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
-// Write your intern objects here:
+// velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
+
+// Using your dinosaur objects, log answers to these questions:
+
+// How much did tyrannosaurus weigh?
+console.log(tyrannosaurus.name);
+// What was the diet of a velociraptor?
+// console.log(velociraptor.diet);
+// // How long was a stegosaurus?
+// console.log(stegosaurus.length);
+// What time period did tyrannosaurus live in?
+console.log(tyrannosaurus.diet);
+
+// Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
+console.log(tyrannosaurus.roar());
 
 
-// ==== Challenge 2: Reading Object Data ==== 
-// Once your objects are created, log out the following requests from HR into the console:
-
-// Mitzi's name
-
-// Kennan's ID
-
-// Keven's email
-
-// Gannie's name
-
-// Antonietta's Gender
-
-// ==== Challenge 3: Object Methods ==== 
-// Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
-
-// Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
-
-// === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
-
-// ==== Stretch Challenge: Nested Objects and the this keyword ==== 
-
-// 1. Create a parent object with properties for name and age.  Make the name Susan and the age 70.
-// 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
-// 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
-// 4. Give each of the objects the ability to speak their names using the this keyword.
-
-const parent = {}
-
-// Log the parent object's name
-
-// Log the child's age
-
-// Log the name and age of the grandchild
-
-// Have the parent speak
-
-// Have the child speak
-
-// Have the grandchild speak
